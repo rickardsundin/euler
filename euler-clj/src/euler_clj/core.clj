b6542de370304d123ecc9c649e6a7dbfe73e938a
Rickard Sundin <rickard@snart.nu>
2015-01-01 21:56:44 +0100

    Solved problem number two.

--- euler-clj/src/euler_clj/core.clj
+++ euler-clj/src/euler_clj/core.clj
@@ -13,3 +13,13 @@
   "Find the sum of all the multiples of 3 or 5 below 1000."
   []
   (apply + (filter #(or (multiple-of? % 3) (multiple-of? % 5)) (take 1000 integers))))
+
+(def fib 
+  "The Fibonacci sequence"
+  (map first (iterate (fn [[a b]] [b (+' a b)]) [0 1])))
+
+(defn problem-2
+  "By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms."
+  []
+  (apply + (filter even? (take-while #(< % 4000000) fib))))
+
--- euler-clj/test/euler_clj/core_test.clj
+++ euler-clj/test/euler_clj/core_test.clj
@@ -5,3 +5,8 @@
 (deftest problem-1-test
   (testing "Multiples of 3 and 5"
     (is (= (problem-1) 233168))))
+
+(deftest problem-2-test
+  (testing "Even Fibonacci numbers"
+    (is (= (problem-2) 4613732))))
+
